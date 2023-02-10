3113eafb98e3bea125d013e1c2ff2347ff9a6d49
Spitap <dev@asdrip.fr>
2023-02-10 15:18:01 +0100

    Fix configuration file limiter

--- modoboa_installer/scripts/fail2ban.py
+++ modoboa_installer/scripts/fail2ban.py
@@ -1,6 +1,9 @@
 """fail2ban related functions."""
 
+import os
+
 from . import base
+from .. import utils
 
 
 class Fail2ban(base.Installer):
@@ -17,11 +20,22 @@ class Fail2ban(base.Installer):
     ]
 
     def get_config_files(self):
+        """Add dovecot and postfix jails if enabled."""
+
         config_files = super().get_config_files()
 
         if self.app_config["postfix_dovecot_filter"].lower() == "true":
             config_files += ["jail.d/postfix.conf",
-                            "jail.d/dovecot.conf", 
-                            "filter.d/postfix-modoboa.conf",
-                            "filter.d/dovecot-modoboa.conf"]
+                            "jail.d/dovecot.conf"]
         return config_files
+
+    def install_config_files(self):
+        """Installer postfix and dovecot filters if enabled."""
+
+        if self.app_config["postfix_dovecot_filter"].lower() == "true":
+            for config_file in ["filter.d/postfix-modoboa.conf",
+                                "filter.d/dovecot-modoboa.conf"]:
+                src = self.get_file_path(config_file)
+                dst = os.path.join(self.config_dir, config_file)
+                utils.copy_file(src, dst)
+        super().install_config_files()
