dfd66e4a21933b56553cd11cf0e49b833d07df1d
A.O <35069360+codebyoul@users.noreply.github.com>
2025-11-29 19:39:34 +0100

    Change restore target to use storage_dir config
    Updated the restore target directory to use storage_dir for data, allowing the user to configure where the data should be stored.

--- modoboa_installer/scripts/radicale.py
+++ modoboa_installer/scripts/radicale.py
@@ -82,7 +82,9 @@ class Radicale(base.Installer):
         radicale_backup = os.path.join(
             self.archive_path, "custom/radicale")
         if os.path.isdir(radicale_backup):
-            restore_target = os.path.join(self.home_dir, "collections")
+            # Use storage_dir for data, fallback to home_dir for compatibility
+            storage_dir = self.config.get("radicale", "storage_dir", fallback=self.home_dir)
+            restore_target = os.path.join(storage_dir, "collections")
             if os.path.isdir(restore_target):
                 shutil.rmtree(restore_target)
             shutil.copytree(radicale_backup, restore_target)
